e413bbb96d5395c2e1506e77e7362204bf3b0adc
unknown <3203317@qq.com>
2017-04-25 23:44:57 +0800

    haha

---
+++ api/base/stock/get/1.2.5.js
@@ -0,0 +1,41 @@
+/*!
+ * oauth2-resource-public
+ * Copyright(c) 2017 huangxin <3203317@qq.com>
+ * ISC Licensed
+ */
+'use strict';
+
+const URL = require('url');
+const request = require('request');
+
+module.exports = function(app, opts){
+  return new Method(app, opts);
+}
+
+var Method = function(app, opts){
+  var self = this;
+  opts = opts || {};
+  self.is_free = true;
+  self.interval_time = opts.interval_time || 30;
+  self.def_num = opts.def_num || 5;
+};
+
+var pro = Method.prototype;
+
+pro.params = function(req, res, next){
+  next();
+};
+
+pro.exec = function(req, res, next){
+  // var result = {
+  //   data: {
+  //     today: !0,
+  //     sys_time: new Date().getTime()
+  //   }
+  // };
+  // res.send(result);
+
+  req.pipe(request('http://hq.sinajs.cn/list=sh601006').on('error', err => {
+    next(err);
+  })).pipe(res);
+};
